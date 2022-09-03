94b4aa753708fc8873a4ebed909a499413f27166
DavitGe <davitge303@gmail.com>
2022-09-03 15:06:34 +0400

    Store data in MYSQL

--- find.php
+++ find.php
@@ -23,17 +23,22 @@
             echo "Failed to connect!";
             exit();
         }else{
-
             //getting data
             $sql = 'SELECT name, id FROM students';
             $result = $conn->query($sql);
-            echo "<br/>" . "=====================================" . "<br/>";
 
-            var_dump($result);
-            echo "<br/>" . "=====================================" . "<br/>";
-            
-
-
+            $output = [];
+            if($result->num_rows > 0) {
+                //check data of each row
+                while($row = $result->fetch_assoc()){
+                    $decoded_row = json_decode(json_encode($row));
+                    if(strtolower($decoded_row->name) === strtolower($_POST['find'])) array_push($output, $decoded_row->id);
+                }
+            }
+            if($output !== []){
+                echo "output data";
+                # output data
+            }else{
             //options for getting content
                 $opts = array(
                     'http'=>array(
@@ -53,12 +58,49 @@
                 function test_studs($stud){
                     $name = strtolower($stud->name);
                     $name_inp = strtolower($_POST['find']);
-                    print_r(str_contains($name, $name_inp));
                     return(str_contains($name, $name_inp));
                 }
 
-                //printing data
-                print_r(array_filter($data, "test_studs"));
+                //saving data
+                $found = array_filter($data, "test_studs");
+                if(!$found){
+                    echo "not found";
+                    #not found design
+                }else{
+
+                    foreach ($found as &$stud) {
+                        $alternate_names = implode($stud->alternate_names);
+                        if ($alternate_names == '') $alternate_names = '[]';
+                        $alternate_actors = implode($stud->alternate_actors);
+                        if ($alternate_actors == '') $alternate_actors = '[]'; 
+                        $wizard = true;
+                        if ($stud->wizard === FALSE) $wizard = 0;
+                        $staff = true;
+                        if ($stud->hogwartsStaff === FALSE) $staff = 0;
+                        $student = true;
+                        if ($stud->hogwartsStudent === FALSE) $student = 0;
+                        $alive = true;
+                        if ($stud->alive === FALSE) $alive = 0;
+
+
+                        $sql = "INSERT INTO `students` (`name`, `alternate_names`, `species`, `gender`, `house`, `dateOfBirth`, `yearOfBirth`, `wizard`, `ancestry`, `eyeColour`, `hairColour`, `wand`, `patronus`, `hogwartsStudent`, `hogwartsStaff`, `actor`, `alternate_actors`, `alive`, `image`) 
+                                VALUES ('$stud->name', '$alternate_names', '$stud->species', '$stud->gender','$stud->house', '$stud->dateOfBirth', '$stud->yearOfBirth', '$wizard', '$stud->ancestry', '$stud->eyeColour', '$stud->hairColour', '$wand', '$stud->patronus', '$student', '$staff', '$stud->actor', '$alternate_actors', '$alive', '$stud->image')";
+                        try{
+                            if($conn->query($sql)){
+                                echo "New record created successfully";
+                            }else{
+                                echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+                        }catch(mysqli_sql_exception $e){
+                            print "<pre>";
+                            var_dump($e);
+                            print "</pre>";
+                            exit;
+                        }
+                    }
+                }   
+
+            }
         }
         ?>
     </main>
